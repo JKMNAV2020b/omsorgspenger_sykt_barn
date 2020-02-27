788438ce41e707ddc8d42e32f5ecb2268658adab
jankaspermartinsen <JKMNAV2020b@gmail.com>
2020-02-27 18:44:44 +0100

    Fjernet overflødig kode Fikset på Kotlin spesifikke unødvendigheter

--- src/main/kotlin/nav/omsorgspenger_sykt_barn/OmsorgspengerSyktBarnController.kt
+++ src/main/kotlin/nav/omsorgspenger_sykt_barn/OmsorgspengerSyktBarnController.kt
@@ -1,7 +1,11 @@
 package nav.omsorgspenger_sykt_barn
 
+import com.google.gson.Gson
+import no.nav.k9.søknad.omsorgspenger.OmsorgspengerSøknad
+import no.nav.k9.søknad.SøknadValidator
 import org.springframework.boot.configurationprocessor.json.JSONObject
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
 
@@ -9,15 +13,19 @@ import org.springframework.web.bind.annotation.*
 @RestController
 class OmsorgspengerSyktBarnController {
 
-    data class Person(val name: String) {
-        var a: Int = 0
-        val x: IntArray = intArrayOf(1, 2, 3)
-    }
 
 
 
-    fun handler(personNummer:String):Person{
-        return Person(personNummer)
+
+
+
+
+
+
+
+
+    fun handler(personNummer:String):String{
+        return personNummer
     }
 
     @GetMapping
@@ -25,12 +33,14 @@ class OmsorgspengerSyktBarnController {
     fun test(@RequestParam(value = "personNummer", defaultValue = "") name: String) = handler(name)
 
     @PostMapping(path = arrayOf("/recreq"), consumes = arrayOf(MediaType.APPLICATION_JSON_VALUE))
-    fun test2(@RequestBody form: String, bindingResult: BindingResult) : String {
-        val jsonObject = JSONObject(form)
-        println(jsonObject.get("søker"))
-        println(form)
-        println(bindingResult)
-        return "{}"
+    fun test2(@RequestBody form: String, bindingResult: BindingResult) : ResponseEntity<Unit> {
+
+        var o:OmsorgspengerSøknad = OmsorgspengerSøknad.SerDes.deserialize(form)
+        println(o.søknadId.id.toString())
+        //public List<Feil> valider(OmsorgspengerSøknad søknad) {
+        //var o:OmsorgspengerSøknadValidator = OmsorgspengerSøknadValidator()
+
+        return ResponseEntity.ok().build()
     }
 
 }
--- src/test/kotlin/nav/omsorgspenger_sykt_barn/OmsorgspengerSyktBarnControllerTest.kt
+++ src/test/kotlin/nav/omsorgspenger_sykt_barn/OmsorgspengerSyktBarnControllerTest.kt
@@ -1,6 +1,10 @@
 package nav.omsorgspenger_sykt_barn
 
+import com.google.gson.Gson
+import no.nav.k9.søknad.omsorgspenger.OmsorgspengerSøknad
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
@@ -10,36 +14,63 @@ import org.springframework.http.MediaType
 import java.net.URI
 
 
-@Autowired
-private val restTemplate: TestRestTemplate? = null
+/*@Autowired
+private val restTemplate: TestRestTemplate? = null*/
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OmsorgspengerSyktBarnControllerTest(@Autowired private val restTemplate: TestRestTemplate) {
 
-
-
-
     @Test
     fun korrekt_json(){
-        var example = "{\"søknadId\":\"XXXX\",\"mottattDato\":\"260676\",\"søker\":\"Per Person\",\"barn\":\"Jens Person\"}"
-        val baseUrl = "http://localhost:8080/recreq";
+        var eksempel = """
+            {
+              "søknadId": "123-123-123",
+              "mottattDato": "2019-10-20T07:15:36.124Z",
+              "versjon": "0.0.1",
+              "søker": {
+                "norskIdentitetsnummer": "11111111111"
+              },
+              "barn": {
+                "fødselsdato": "2015-01-01",
+                "norskIdentitetsnummer" : null
+              }
+            }
+        """.trimIndent()
+        val baseUrl = "http://localhost:8080/recreq"
         val uri = URI(baseUrl)
-        val headers: HttpHeaders = HttpHeaders()
+        val headers = HttpHeaders()
         headers.setContentType(MediaType.APPLICATION_JSON)
-        val request: HttpEntity<String> = HttpEntity<String>(example, headers)
-        val result = restTemplate!!.postForEntity(uri, request, String::class.java)
+        val request: HttpEntity<String> = HttpEntity<String>(eksempel, headers)
+        val result = restTemplate.postForEntity(uri, request, String::class.java)
+
         assert(result.statusCodeValue == 200)
     }
 
     @Test
     fun misformet_json_skal_stoppe(){
-        var example = "{\"søknadId\":\"XXXX\",\"mottattDato\":\"260676\",\"søker\":\"Per Person\"\"barn\":\"Jens Person\""
-        val baseUrl = "http://localhost:8080/recreq";
+        val example = "{\"søknadId\":\"XXXX\",\"mottattDato\":\"260676\",\"søker\":\"Per Person\"\"barn\":\"Jens Person\""
+        val baseUrl = "http://localhost:8080/recreq"
         val uri = URI(baseUrl)
-        val headers: HttpHeaders = HttpHeaders()
+        val headers = HttpHeaders()
         headers.setContentType(MediaType.APPLICATION_JSON)
         val request: HttpEntity<String> = HttpEntity<String>(example, headers)
-        val result = restTemplate!!.postForEntity(uri, request, String::class.java)
+        val result = restTemplate.postForEntity(uri, request, String::class.java)
         assert(result.statusCodeValue == 500)
+    }
+
+    @Test
+    fun feiler_med_GSON(){
+
+        var eksempelSøknad = """{"søknadId":"123-123-123","mottattDato":"2019-10-20T07:15:36.124Z","versjon":"0.0.1","søker":{"norskIdentitetsnummer":"11111111111"},"barn":{"fødselsdato":"2015-01-01","norskIdentitetsnummer":"null"}}"""
+
+        assertThrows<com.google.gson.JsonSyntaxException>{
+            Gson().fromJson(eksempelSøknad,OmsorgspengerSøknad::class.java)
+        }
+        //should continue to investigate why this is a problem....even though it is not a relevant problem
+        assertDoesNotThrow {
+            eksempelSøknad = eksempelSøknad.replace("ø","oe")
+            println(eksempelSøknad)
+            var omsorgspengerSøknad = Gson().fromJson("""{"soeknadId":"123-123-123","mottattDato":"2019"}""",OmsorgspengerSøknad::class.java)
+        }
     }
 }
