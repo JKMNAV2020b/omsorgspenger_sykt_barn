971662c7bd53f6ca3e1202f54f3849fa4580c867
jankaspermartinsen <JKMNAV2020b@gmail.com>
2020-02-28 13:18:51 +0100

    gi nytt navn (1)

--- src/main/kotlin/nav/omsorgspenger_sykt_barn/OmsorgspengerSyktBarnController.kt
+++ src/main/kotlin/nav/omsorgspenger_sykt_barn/OmsorgspengerSyktBarnController.kt
@@ -24,7 +24,7 @@ class OmsorgspengerSyktBarnController {
     @PostMapping(path = arrayOf("/recreq"), consumes = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     fun test2(@RequestBody form: String, bindingResult: BindingResult) : ResponseEntity<Unit> {
 
-        var o:OmsorgspengerSøknad = OmsorgspengerSøknad.SerDes.deserialize(form)
+        val o:OmsorgspengerSøknad = OmsorgspengerSøknad.SerDes.deserialize(form)
         println(o.søknadId.id.toString())
         //public List<Feil> valider(OmsorgspengerSøknad søknad) {
         //var o:OmsorgspengerSøknadValidator = OmsorgspengerSøknadValidator()
@@ -32,4 +32,34 @@ class OmsorgspengerSyktBarnController {
         return ResponseEntity.ok().build()
     }
 
+    @PostMapping(path = arrayOf("/utvidetrett"), consumes = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    fun utvidrett(@RequestBody body: String):ResponseEntity<Unit> {
+        return ResponseEntity.ok().build()
+    }
+
+    @PostMapping(path = arrayOf("/midlertidigalene"), consumes = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    fun midlertidalene(@RequestBody body: String):ResponseEntity<Unit> {
+
+        println(body)
+        return ResponseEntity.ok().build()
+    }
+
+    // to usecaes for å overføre dager
+    // 1. overføre samværesdager til barnets far/mor
+    // 2. overføre samværesdager til ny kone/samboer/kjæreste
+
+    @PostMapping(path = arrayOf("/overførdager"), consumes = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    fun overførDager(@RequestBody body: String):ResponseEntity<Unit> {
+        return ResponseEntity.ok().build()
+    }
+
+    @PostMapping(path = arrayOf("/fordeldager"), consumes = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    fun fordelDager(@RequestBody body: String):ResponseEntity<Unit> {
+        return ResponseEntity.ok().build()
+    }
+
+    @PostMapping(path = arrayOf("/utbetal"), consumes = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    fun utbetal(@RequestBody body: String):ResponseEntity<Unit> {
+        return ResponseEntity.ok().build()
+    }
 }
