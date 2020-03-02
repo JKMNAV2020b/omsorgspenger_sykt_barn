6c91ec41955787700a281e7b3cbee57e7e4277c4
jankaspermartinsen <JKMNAV2020b@gmail.com>
2020-03-02 13:07:01 +0100

    Lagde et mer fornuftig katalogstruktur

---
+++ app/src/main/kotlin/no/nav/k9/aarskvantum/K9AarskvantumApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.k9.aarskvantum
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class K9AarskvantumApplication
+
+fun main(args: Array<String>) {
+    runApplication<K9AarskvantumApplication>(*args)
+}
---
+++ app/src/test/kotlin/no/nav/k9/aarskvantum/K9AarskvantumApplicationTest.kt
@@ -0,0 +1,13 @@
+package nav.omsorgspenger_sykt_barn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class K9AarskvantumApplicationTest {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
