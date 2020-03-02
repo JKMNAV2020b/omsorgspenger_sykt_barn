2ea57a6e2f4175745e0004fcff852e3c5b9c5a10
jankaspermartinsen <JKMNAV2020b@gmail.com>
2020-03-02 15:35:11 +0100

    fant alder på barn utifra rest melding

--- app/src/main/kotlin/no/nav/k9/aarskvantum/controller/OmsorgspengerSyktBarnController.kt
+++ app/src/main/kotlin/no/nav/k9/aarskvantum/controller/OmsorgspengerSyktBarnController.kt
@@ -5,6 +5,14 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.Period
+import java.time.format.DateTimeFormatter
+
+
+
+
 
 @RestController
 class OmsorgspengerSyktBarnController {
@@ -16,8 +24,40 @@ class OmsorgspengerSyktBarnController {
     @PostMapping(path = arrayOf("/recreq"), consumes = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     fun test2(@RequestBody form: String, bindingResult: BindingResult) : ResponseEntity<Unit> {
 
-        val o: OmsorgspengerSøknad = OmsorgspengerSøknad.SerDes.deserialize(form)
-        println(o.søknadId.id.toString())
+        try {
+            val o: OmsorgspengerSøknad = OmsorgspengerSøknad.SerDes.deserialize(form)
+            println(o.søknadId.id.toString())
+
+
+            /*
+            * 1. sjekke om brukeren finnes i databasen, eller sjekke om databasen er der i det heletatt...
+            *
+            * */
+
+            println(o.barn.fødselsdato)
+            val current = LocalDateTime.now()
+            println(o.barn.fødselsdato.atStartOfDay())
+
+            // fant ut om brukeren som ble sendt inn var under eller lik 12.
+
+            var date = LocalDate.parse(o.barn.fødselsdato.toString())
+            println(Period.between(o.barn.fødselsdato,current.toLocalDate()).years)
+
+
+
+
+
+
+
+
+
+
+        }catch (e:Exception){
+            println(e.toString())
+        }
+
+
+
         return ResponseEntity.ok().build()
     }
 
